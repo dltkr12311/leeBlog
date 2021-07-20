abb453beff9aa5425261b56508a1baac3d2b38ae
dltkr12311 <leesak94@gmail.com>
2021-07-20 20:36:08 +0900

    apply styled emotion CSS in JS 방식 적용함

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React, { Fragment, FunctionComponent } from 'react';
-import Text from 'components/Text';
 import { Link } from 'gatsby';
 
 const IndexPage: FunctionComponent = () => {
   return (
     <Fragment>
-      <Text text="HOME" />
       <Link to="/info/">To Info</Link>
     </Fragment>
   );
